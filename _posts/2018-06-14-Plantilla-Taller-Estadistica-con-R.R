5c4b3fcf465426cafc03d854549759d5f7a8c5e0
Mucio Osorio <mucio.osorio@itson.edu.mx>
2018-06-13 19:30:34 -0700

    Update 2018-06-14-Plantilla-Taller-Estadistica-con-R.R

--- _posts/2018-06-14-Plantilla-Taller-Estadistica-con-R.R
+++ _posts/2018-06-14-Plantilla-Taller-Estadistica-con-R.R
@@ -1,90 +1,77 @@
-# TALLER ESTADISTICA CON R
-################################################################################
+ TALLER ESTADISTICA CON R
 
 
+ R COMO CALCULADORA
 
-# R COMO CALCULADORA
+ ASIGNACION DE VALORES
 
-# ASIGNACION DE VALORES
-
-
-# COMO LEER DATOS (Concatenar e importar)
+ COMO LEER DATOS (Concatenar e importar)
 
 
-# ESTADISTICA DESCRIPTIVA
-################################################################################
+ ESTADISTICA DESCRIPTIVA
 
-# 1. Localización de los datos
-################################
+ 1. Localización de los datos
 
-# Deben ubicarse el mínimo y el máximo de los datos, 
-# calcularse las medidas de tendencia central y los percentiles.
+ Deben ubicarse el mínimo y el máximo de los datos, calcularse las medidas de tendencia central y los percentiles.
 
 
-# 2. Variabilidad
-################################
-
-# Las medidas de localización deben acompañarse con una medida 
-# de la variación de los datos. Usualmente se calculan la amplitud,  
-# varianza y desviación estándar.
+ 2. Variabilidad
 
 
-# 3. Distribución de las frecuencias
-################################
-
-# La forma que tiene la distribución de las frecuencias de los datos es 
-# importante por ejemplo al seleccionar la medida de tendencia central a usar 
-# o la prueba estádistica que aplica para ese tipo de datos.
-
-# El gráfico útil para ver la forma de los datos es el histograma
-
-# En las variables nominales son útiles los gráficos de barras y los de pastel
-
-# En el caso de variables cualitativas, puede construirse un gráfico de barras 
-# o uno de pastel
-
-# 4. Valores extremos
-##############################
-
-# Los valores muy grandes o muy pequeños afectan a las medidas de localización 
-# y a las de variabilidad, por esto, es importante verificar la existencia 
-# de éstos valores que en Estadística se denominan "extremos". Hay varios 
-# métodos para detectar valores extremos, uno de ellos son los **boxplot** 
-# o **graficos de caja y bigote**.
-
-# 5. Relación entre variables.
-###############################
-
-# Es importante en ocasiones estudiar la relación que existe entre las 
-# variables de interés. En éstas situaciones el diagrama de dispersión 
-# y el coeficiente de correlación son dos herramientas indispensables.
+ Las medidas de localización deben acompañarse con una medida de la variación de los datos. 
+ Usualmente se calculan la amplitud, varianza y desviación estándar.
 
 
-# La función summary() da un resumen descriptivo de los datos.
+ 3. Distribución de las frecuencias
 
-# ¿Qué pasa cuando hay valores faltantes?
+ La forma que tiene la distribución de las frecuencias de los datos es importante por ejemplo al seleccionar la medida
+ de tendencia central a usar o la prueba estádistica que aplica para ese tipo de datos.
 
-# En muchas ocasiones (casi siempre :) ) hay datos faltantes. 
-# Automaticamente, en R , toda celda con las letras NA es considerada un valor ausente.
-# Para el uso de algunas funciones se debe informar a R de la existencia 
-# de valores extraviados.
+ El gráfico útil para ver la forma de los datos es el histograma
 
-# INFERENCIA ESTADISTICA
-################################################################################
+ En las variables nominales son útiles los gráficos de barras y los de pastel
 
-# INTERVALOS DE CONFIANZA DE UN PROMEDIO POBLACIONAL
+ En el caso de variables cualitativas, puede construirse un gráfico de barras 
+ o uno de pastel
 
-# INTERVALO DE CONFIANZA DE UNA PROPORCIÓN POBLACIONAL
+ 4. Valores extremos
 
-# INTERVALO DE CONFIANZA PARA LA DIFERENCIA DE DOS PROMEDIOS POBLACIONALES
+ Los valores muy grandes o muy pequeños afectan a las medidas de localización 
+ y a las de variabilidad, por esto, es importante verificar la existencia 
+ de éstos valores que en Estadística se denominan "extremos". Hay varios 
+ métodos para detectar valores extremos, uno de ellos son los boxplot 
+ o graficos de caja y bigote.
 
-# INTERVALO DE CONFIANZA PARA LA DIFERENCIA DE DOS PROPORCIONESPOBLACIONALES
+ 5. Relación entre variables.
 
-# PRUEBA DE HIPÓTESIS DE UN PROMEDIO POBLACIONAL
+ Es importante en ocasiones estudiar la relación que existe entre las 
+ variables de interés. En éstas situaciones el diagrama de dispersión 
+ y el coeficiente de correlación son dos herramientas indispensables.
 
-# PRUEBA DE HIPÓTESIS DE DOS PROMEDIOS POBLACIONALES
+ La función summary() da un resumen descriptivo de los datos.
 
-# REGRESION LINEAL
+ ¿Qué pasa cuando hay valores faltantes?
+
+ En muchas ocasiones (casi siempre :) ) hay datos faltantes. 
+ Automaticamente, en R , toda celda con las letras NA es considerada un valor ausente.
+ Para el uso de algunas funciones se debe informar a R de la existencia 
+ de valores extraviados.
+
+ INFERENCIA ESTADISTICA
+
+ INTERVALOS DE CONFIANZA DE UN PROMEDIO POBLACIONAL
+
+ INTERVALO DE CONFIANZA DE UNA PROPORCIÓN POBLACIONAL
+
+ INTERVALO DE CONFIANZA PARA LA DIFERENCIA DE DOS PROMEDIOS POBLACIONALES
+
+ INTERVALO DE CONFIANZA PARA LA DIFERENCIA DE DOS PROPORCIONESPOBLACIONALES
+
+ PRUEBA DE HIPÓTESIS DE UN PROMEDIO POBLACIONAL
+
+ PRUEBA DE HIPÓTESIS DE DOS PROMEDIOS POBLACIONALES
+
+ REGRESION LINEAL
 
 
 
